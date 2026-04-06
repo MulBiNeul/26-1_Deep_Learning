1937577519a72d2dc4350a8279580d04c9743582
MulBiNeul <hello88bgm@gmail.com>
2026-04-06 11:45:49 +0900

    Remove

--- open-vocabulary_detection/src/utils/config.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-
-def load_config(config_path: str) -> dict: 
-    """
-    Load a YAML config file and return it as a dictionary.
-    """
-    with open(config_path, "r", encoding="utf-8") as f:
-        return yaml.safe_load(f)
